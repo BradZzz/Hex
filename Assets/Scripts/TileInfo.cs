ac308986eae86325dc81d02b65140408e204b431
Brad <brad@lax-m10517.home>
2018-05-21 01:41:02 -0700

    added adventure scene

---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+[Serializable]
+public class TileInfo {
+	
+	//The type of unit the player is using
+	public tileType type;
+	//How many actions this unit has left
+	public string color = "";
+	//How many attacks this unit has left
+	public int movement = 0;
+
+	public string meta = "";
+
+	public enum tileType {
+		Castle, City, Forest, Grass, Mountain, Treasure, Water,  None
+	}
+}
