9f33f60b15ea3f48143f94c8e7323adaa8607944
Brad <brad@lax-m10517.home>
2018-05-25 13:06:09 -0700

    added bounding box for choices panel

--- Assets/Scripts/ChoiceScene/ChoicePanel.cs
+++ Assets/Scripts/ChoiceScene/ChoicePanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChoicePanel : MonoBehaviour {
 
-	public float speed = 20;
+//	public float speed = 20;
 	public GameObject panelParent;
 	public GameObject player;
 
@@ -19,42 +19,42 @@ public class ChoicePanel : MonoBehaviour {
 		thisPlayer.transform.localScale = new Vector3(200, 200, 1);
 	}
 
-	void Update()
-	{
-		float mv = speed * Time.deltaTime;
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			thisPlayer.transform.Translate(new Vector3(mv,0,0));
-		}
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			thisPlayer.transform.Translate(new Vector3(-mv,0,0));
-		}
-		if(Input.GetKey(KeyCode.DownArrow))
-		{
-			thisPlayer.transform.Translate(new Vector3(0,-mv,-mv));
-		}
-		if(Input.GetKey(KeyCode.UpArrow))
-		{
-			thisPlayer.transform.Translate(new Vector3(0,mv,mv));
-		}
-	}
-
-	private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
-	{
-		Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
-		Color[] rpixels = result.GetPixels(0);
-		float incX = (1.0f / (float)targetWidth);
-		float incY = (1.0f / (float)targetHeight);
-		for (int px = 0; px < rpixels.Length; px++)
-		{
-			rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
-		}
-		result.SetPixels(rpixels, 0);
-		result.Apply();
-		return result;
-
-	}
+//	void Update()
+//	{
+//		float mv = speed * Time.deltaTime;
+//		if(Input.GetKey(KeyCode.RightArrow))
+//		{
+//			thisPlayer.transform.Translate(new Vector3(mv,0,0));
+//		}
+//		if(Input.GetKey(KeyCode.LeftArrow))
+//		{
+//			thisPlayer.transform.Translate(new Vector3(-mv,0,0));
+//		}
+//		if(Input.GetKey(KeyCode.DownArrow))
+//		{
+//			thisPlayer.transform.Translate(new Vector3(0,-mv,-mv));
+//		}
+//		if(Input.GetKey(KeyCode.UpArrow))
+//		{
+//			thisPlayer.transform.Translate(new Vector3(0,mv,mv));
+//		}
+//	}
+//
+//	private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+//	{
+//		Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
+//		Color[] rpixels = result.GetPixels(0);
+//		float incX = (1.0f / (float)targetWidth);
+//		float incY = (1.0f / (float)targetHeight);
+//		for (int px = 0; px < rpixels.Length; px++)
+//		{
+//			rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+//		}
+//		result.SetPixels(rpixels, 0);
+//		result.Apply();
+//		return result;
+//
+//	}
 
 //	// Use this for initialization
 //	void Start () {
---
+++ Assets/Scripts/ChoiceScene/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	public float speed = 100f;
+
+	void Update()
+	{
+		/*
+		 * Check here to make sure the player cannot leave the box
+		 */
+
+		float mv = speed * Time.deltaTime;
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(mv,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-mv,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(new Vector3(0,-mv,0));
+			//transform.Translate(new Vector3(0,-mv,-mv));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0,mv,0));
+			//transform.Translate(new Vector3(0,mv,mv));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision : " + collision.gameObject.name);
+	}
+
+	//Detect when there is a collision starting
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		//Ouput the Collision to the console
+		Debug.Log("Collision : " + collision.gameObject.name);
+	}
+
+	//Detect when there is are ongoing Collisions
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		//Output the Collision to the console
+		Debug.Log("Stay : " + collision.gameObject.name);
+	}
+
+}
