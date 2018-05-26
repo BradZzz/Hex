80e118ea06ee06ce66827d21ea0c1c387402c3bb
Brad <brad@lax-m10517.home>
2018-05-26 00:30:05 -0700

    for save

--- Assets/Scripts/ChoiceScene/ChoicePanel.cs
+++ Assets/Scripts/ChoiceScene/ChoicePanel.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChoicePanel : MonoBehaviour {
 
 	public enum minigameType {
-		Shoot, Town, None
+		Pop, Shoot, Town, None
 	}
 
 	public GameObject panelParent;
@@ -13,11 +13,18 @@ public class ChoicePanel : MonoBehaviour {
 	public GameObject navPlayer;
 	public GameObject enemy;
 	public GameObject shop;
+	public GameObject clickable;
 
 	public minigameType gameType;
 
+	public float gameTimeLimit = 20f;
+	private float gameTimer;
+
 	public float enemyGenSpeed = 5f;
 	private float enemyGenTimer = 0;
+
+	public float clickableGenSpeed = 5f;
+	private float clickableGenTimer = 0;
 
 	private GameObject thisPlayer;
 	private int windowH;
@@ -29,24 +36,33 @@ public class ChoicePanel : MonoBehaviour {
 		windowH = (int)panelParent.GetComponent<RectTransform> ().rect.height;
 		windowW = (int)panelParent.GetComponent<RectTransform> ().rect.width;
 
-		if (gameType == minigameType.Shoot) {
-			thisPlayer = Instantiate(player);
-		} else if (gameType == minigameType.Town) {
-			thisPlayer = Instantiate(navPlayer);
-		}
-		thisPlayer.transform.SetParent(panelParent.transform, false);
-		thisPlayer.transform.localPosition = new Vector3(0,-windowH/4-50,0);
-		thisPlayer.transform.localScale = new Vector3(200, 200, 1);
+		gameTimer = gameTimeLimit;
 
-		if (gameType == minigameType.Town) {
-			for (int i = 0; i < 20; i++) {
-				genStore ();
+		if (gameType == minigameType.Shoot || gameType == minigameType.Town) {
+			switch(gameType) {
+			case minigameType.Shoot:
+				thisPlayer = Instantiate(player);
+				break;
+			case minigameType.Town:
+				thisPlayer = Instantiate(navPlayer);
+				break;
+			}
+			thisPlayer.transform.SetParent(panelParent.transform, false);
+			thisPlayer.transform.localPosition = new Vector3(0,-windowH/4-50,0);
+			thisPlayer.transform.localScale = new Vector3(200, 200, 1);
+
+			if (gameType == minigameType.Town) {
+				for (int i = 0; i < 20; i++) {
+					genStore ();
+				}
 			}
 		}
 	}
 
 	void Update()
 	{
+		gameTimer -= Time.deltaTime;
+
 		if (minigameType.Shoot == gameType) {
 			enemyGenTimer -= Time.deltaTime;
 			if(enemyGenTimer < 0)
@@ -56,6 +72,74 @@ public class ChoicePanel : MonoBehaviour {
 			}
 		}
 
+		if (minigameType.Pop == gameType) {
+			clickableGenTimer -= Time.deltaTime;
+			if(clickableGenTimer < 0)
+			{
+				for (int i = 0; i < 7; i++) {
+					if (i < 2) {
+						genClickable (Color.green);
+					} else {
+						genClickable (Color.red);
+					}
+				}
+				clickableGenTimer = clickableGenSpeed;
+			}
+		}
+	}
+
+	private static Texture2D _staticRectTexture;
+	private static GUIStyle _staticRectStyle;
+
+	private static Texture2D _staticHealthTexture;
+	private static GUIStyle _staticHealthStyle;
+
+	void OnGUI() {
+		if (gameTimer > 0) {
+			RectTransform boxRect = panelParent.GetComponent<RectTransform>();
+			Vector3 guiPosition = transform.position;
+			guiPosition.x -= 4 * boxRect.rect.width / 10;
+			guiPosition.y += 5.5f * boxRect.rect.height / 10;
+
+			float bxWidth = 8.94f * boxRect.rect.width / 10;
+
+			//Black Box Base
+			Rect bRect = new Rect (guiPosition.x - 18, guiPosition.y - 38, bxWidth + 4, 32);
+
+			if (_staticRectTexture == null) {
+				_staticRectTexture = new Texture2D (1, 1);
+			}
+			if (_staticRectStyle == null) {
+				_staticRectStyle = new GUIStyle ();
+			}
+
+			_staticRectTexture.SetPixel (0, 0, Color.black);
+			_staticRectTexture.Apply ();
+
+			_staticRectStyle.normal.background = _staticRectTexture;
+
+			GUI.Box (bRect, GUIContent.none, _staticRectStyle);
+
+			//Health Overlay
+			Rect hRect = new Rect (guiPosition.x - 16, guiPosition.y - 35, 
+				bxWidth * (float)gameTimer / (float)gameTimeLimit, 26);
+
+			if (_staticHealthTexture == null) {
+				_staticHealthTexture = new Texture2D (1, 1);
+			}
+			if (_staticHealthStyle == null) {
+				_staticHealthStyle = new GUIStyle ();
+			}
+
+			_staticHealthTexture.SetPixel (0, 0, Color.yellow);
+			_staticHealthTexture.Apply ();
+
+			_staticHealthStyle.normal.background = _staticHealthTexture;
+
+			GUI.Box (hRect, GUIContent.none, _staticHealthStyle);
+
+			Debug.Log (guiPosition.ToString());
+			}
 	}
 
 	void genEnemy() {
@@ -75,5 +159,31 @@ public class ChoicePanel : MonoBehaviour {
 		thisShop.transform.SetParent(panelParent.transform, false);
 		thisShop.transform.localPosition = new Vector3(x,y,0);
 		thisShop.transform.localScale = new Vector3(200, 200, 1);
+	}
+
+	void genClickable(Color meshColor) {
+		float x = Random.Range (-windowW / 3, windowW / 3);
+		float y = Random.Range (-windowH / 3, windowH / 3);
+
+		GameObject thisClick = Instantiate(clickable);
+		thisClick.transform.SetParent(panelParent.transform, false);
+		thisClick.transform.localPosition = new Vector3(x,y,0);
+		thisClick.transform.localScale = new Vector3(300, 300, 1);
+
+		thisClick.GetComponent<SpriteRenderer> ().color = meshColor;
+		if (meshColor == Color.red) {
+			thisClick.tag = "Enemy";
+//			thisClick.transform.localScale = new Vector3 (100, 100, 1);
+//
+//			Vector3 temp = thisClick.GetComponent<RectTransform>().localScale;
+//			temp.x = temp.x / 2;
+//			temp.y = temp.y / 2;
+//			thisClick.GetComponent<RectTransform>().localScale = new Vector3(10f,10f,1);
+		} else {
+			thisClick.transform.localScale = new Vector3 (150, 150, 1);
+		}
+		thisClick.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range (-.1f,.1f),Random.Range (-.1f,.1f)),ForceMode2D.Force);
+
+		//thisClick.transform.rotation = Quaternion.Euler(Random.Range (-1, 1), Random.Range (-1, 1), Random.Range (-1, 1));
 	}
 }
---
+++ Assets/Scripts/ChoiceScene/Popper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popper : MonoBehaviour {
+
+	public float speed = .2f;
+	public int health = 3;
+
+//	void Update () {
+//		transform.Translate(new Vector3(0,speed,0));
+//	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision : " + collision.gameObject.name);
+		Debug.Log ("Tag: " + collision.gameObject.tag);
+		if (collision.gameObject.tag.Equals("Wall")) {
+			Vector2 forceVec = -GetComponent<Rigidbody2D>().velocity.normalized * 20f;
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range (-.1f,.1f),Random.Range (-.1f,.1f)),ForceMode2D.Force);
+		} else if (collision.gameObject.tag.Equals("Enemy") && !gameObject.tag.Equals("Enemy")) {
+			health--;
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	//Detect when there is a collision starting
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		//Ouput the Collision to the console
+		Debug.Log("Collision : " + collision.gameObject.name);
+	}
+
+	//Detect when there is are ongoing Collisions
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		//Output the Collision to the console
+		Debug.Log("Stay : " + collision.gameObject.name);
+	}
+
+	void OnMouseDown(){
+		if (gameObject.tag.Equals("Enemy")) {
+			Destroy (this.gameObject);
+		}
+	}   
+}
