fabe9f8f9244c2095f72e0c8fa0acdb15151e45a
Brad <brad@lax-m10517.local>
2018-06-28 17:37:04 -0700

    added skyboxes

---
+++ Assets/Scripts/Utils/DynamicSorting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class DynamicSorting : MonoBehaviour
+{
+  void LateUpdate()
+  {
+    var renderer = GetComponent<Renderer>();
+    renderer.sortingOrder = -(int)(transform.position.y * 1000.0f);
+  }
+}
